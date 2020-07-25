824034e796366c07390cde1151ca9207a9b99f4c
Andy Maleh <andy.am@gmail.com>
2020-07-25 05:01:16 -0400

    Added parent to debug message in widget_listener_expression

--- lib/glimmer/dsl/swt/widget_listener_expression.rb
+++ lib/glimmer/dsl/swt/widget_listener_expression.rb
@@ -11,7 +11,7 @@ module Glimmer
           Glimmer::Config.logger.debug {"keyword starts with on_: #{keyword.start_with?('on_')}"}
           return false unless keyword.start_with?('on_')
           widget_or_display_parentage = parent.respond_to?(:swt_widget) || parent.is_a?(Glimmer::SWT::DisplayProxy)
-          Glimmer::Config.logger.debug {"parent is a widget or display: #{widget_or_display_parentage}"}
+          Glimmer::Config.logger.debug {"parent #{parent} is a widget or display: #{widget_or_display_parentage}"}
           return false unless widget_or_display_parentage
           Glimmer::Config.logger.debug {"block exists?: #{!block.nil?}"}
           raise Glimmer::Error, "Listener is missing block for keyword: #{keyword}" unless block_given?
