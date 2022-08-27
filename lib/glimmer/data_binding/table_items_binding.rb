734c29735766b38bc6c3299ee9cb7d2c60442405
Andy Maleh <andy.am@gmail.com>
2022-08-26 21:26:55 -0400

    Optimize table data-binding performance by not updating all rows for every change when models have not changed

--- lib/glimmer/data_binding/table_items_binding.rb
+++ lib/glimmer/data_binding/table_items_binding.rb
@@ -65,22 +65,34 @@ module Glimmer
         Glimmer::SWT::DisplayProxy.instance.auto_exec(override_sync_exec: @model_binding.binding_options[:sync_exec], override_async_exec: @model_binding.binding_options[:async_exec]) do
           new_model_collection = model_binding_evaluated_property = @model_binding.evaluate_property unless internal_sort # this ensures applying converters (e.g. :on_read)
           return if same_table_data?(new_model_collection)
-          if new_model_collection and new_model_collection.is_a?(Array)
-            remove_dependent(@table_observer_registration => @table_items_observer_registration) if @table_items_observer_registration
-            @table_items_observer_registration&.unobserve
-            @table_items_observer_registration = observe(new_model_collection, @column_properties)
-            add_dependent(@table_observer_registration => @table_items_observer_registration)
-            @table_items_property_observer_registration ||= {}
-            TABLE_ITEM_PROPERTIES.each do |table_item_property|
-              remove_dependent(@table_observer_registration => @table_items_property_observer_registration[table_item_property]) if @table_items_property_observer_registration[table_item_property]
-              @table_items_property_observer_registration[table_item_property]&.unobserve
-              property_properties = @column_properties.map {|property| "#{property}_#{table_item_property}" }
-              @table_items_property_observer_registration[table_item_property] = observe(new_model_collection, property_properties)
-              add_dependent(@table_observer_registration => @table_items_property_observer_registration[table_item_property])
+          if same_model_collection?(new_model_collection)
+            new_model_collection_attribute_values = model_collection_attribute_values(new_model_collection)
+            @table.swt_widget.items.each_with_index do |table_item, i|
+              next if @last_model_collection_attribute_values[i] == new_model_collection_attribute_values[i]
+              model = table_item.get_data
+              for index in 0..(@column_properties.size-1)
+                model_attribute = @column_properties[index]
+                update_table_item_properties_from_model(table_item, index, model, model_attribute)
+              end
             end
-            @model_collection = new_model_collection
+          else
+            if new_model_collection and new_model_collection.is_a?(Array)
+              remove_dependent(@table_observer_registration => @table_items_observer_registration) if @table_items_observer_registration
+              @table_items_observer_registration&.unobserve
+              @table_items_observer_registration = observe(new_model_collection, @column_properties)
+              add_dependent(@table_observer_registration => @table_items_observer_registration)
+              @table_items_property_observer_registration ||= {}
+              TABLE_ITEM_PROPERTIES.each do |table_item_property|
+                remove_dependent(@table_observer_registration => @table_items_property_observer_registration[table_item_property]) if @table_items_property_observer_registration[table_item_property]
+                @table_items_property_observer_registration[table_item_property]&.unobserve
+                property_properties = @column_properties.map {|property| "#{property}_#{table_item_property}" }
+                @table_items_property_observer_registration[table_item_property] = observe(new_model_collection, property_properties)
+                add_dependent(@table_observer_registration => @table_items_property_observer_registration[table_item_property])
+              end
+              @model_collection = new_model_collection
+            end
+            populate_table(@model_collection, @table, @column_properties, internal_sort: internal_sort)
           end
-          populate_table(@model_collection, @table, @column_properties, internal_sort: internal_sort)
         end
       end
       
@@ -93,26 +105,31 @@ module Glimmer
           table_item = TableItem.new(parent.swt_widget, SWT::SWTProxy[:none])
           for index in 0..(column_properties.size-1)
             model_attribute = column_properties[index]
-            table_item.setText(index, model.send(model_attribute).to_s)
-            TABLE_ITEM_PROPERTIES.each do |table_item_property|
-              if model.respond_to?("#{model_attribute}_#{table_item_property}")
-                table_item_value = model.send("#{model_attribute}_#{table_item_property}")
-                if table_item_value
-                  table_item_value = Glimmer::SWT::ColorProxy.create(*table_item_value).swt_color if %w[background foreground].include?(table_item_property.to_s)
-                  table_item_value = Glimmer::SWT::FontProxy.new(table_item_value).swt_font if table_item_property.to_s == 'font'
-                  table_item_value = Glimmer::SWT::ImageProxy.create(*table_item_value).swt_image if table_item_property.to_s == 'image'
-                  table_item.send("set_#{table_item_property}", index, table_item_value)
-                end
-              end
-            end
+            update_table_item_properties_from_model(table_item, index, model, model_attribute)
           end
           table_item.set_data(model)
         end
+        @last_model_collection_attribute_values = model_collection_attribute_values(model_collection)
         selected_table_items = parent.search {|item| selected_table_item_models.include?(item.get_data) }
         parent.swt_widget.setSelection(selected_table_items)
         sorted_model_collection = parent.sort!(internal_sort: internal_sort)
         call(sorted_model_collection, internal_sort: true) if @read_only_sort && !internal_sort && !sorted_model_collection.nil?
         parent.swt_widget.redraw if parent&.swt_widget&.respond_to?(:redraw)
+      end
+      
+      def update_table_item_properties_from_model(table_item, index, model, model_attribute)
+        table_item.setText(index, model.send(model_attribute).to_s)
+        TABLE_ITEM_PROPERTIES.each do |table_item_property|
+          if model.respond_to?("#{model_attribute}_#{table_item_property}")
+            table_item_value = model.send("#{model_attribute}_#{table_item_property}")
+            if table_item_value
+              table_item_value = Glimmer::SWT::ColorProxy.create(*table_item_value).swt_color if %w[background foreground].include?(table_item_property.to_s)
+              table_item_value = Glimmer::SWT::FontProxy.new(table_item_value).swt_font if table_item_property.to_s == 'font'
+              table_item_value = Glimmer::SWT::ImageProxy.create(*table_item_value).swt_image if table_item_property.to_s == 'image'
+              table_item.send("set_#{table_item_property}", index, table_item_value)
+            end
+          end
+        end
       end
       
       def same_table_data?(new_model_collection)
@@ -146,6 +163,23 @@ module Glimmer
           table_cells == model_cells
         end
       end
+      
+      def same_model_collection?(new_model_collection)
+        new_model_collection == table_item_model_collection
+      end
+      
+      def table_item_model_collection
+        @table.swt_widget.items.map(&:get_data)
+      end
+      
+      def model_collection_attribute_values(model_collection)
+        model_collection.map do |model|
+          (["text"] + TABLE_ITEM_PROPERTIES).map do |table_item_property|
+            @table.cells_for(model, table_item_property: table_item_property)
+          end.reduce(:+)
+        end
+      end
+      
     end
   end
 end
